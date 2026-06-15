c73cec88c01beb89cf7095b62954c6c75c949448
kellywong02 <86651931+kellywong02@users.noreply.github.com>
2026-06-15 13:54:00 +0800

    Updated Server.js

--- server.js
+++ server.js
@@ -724,3 +724,11 @@ function getImageExtension(contentType, imageUrl) {
 
     return 'jpg';
 }
+
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
